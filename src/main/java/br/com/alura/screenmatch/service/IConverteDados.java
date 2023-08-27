389b9676e7640d1e7b4e36f68acdcffa84e62648
matth <mattheew.oliveira@gmail.com>
2023-08-27 11:32:59 -0300

    Inicio projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch/service/IConverteDados.java
@@ -0,0 +1,10 @@
+package br.com.alura.screenmatch.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public interface IConverteDados {
+
+   <T> T obterDados(String json, Class<T> classe) throws JsonProcessingException;
+
+
+}
